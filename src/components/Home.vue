d1f261670521817e5c7393467464eb86538082f6
duan <xin.duan@echocloud.com>
2019-10-15 11:57:26 +0800

    add download

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <input type="file" ref="upload" accept=".xls,.xlsx" class="outputlist_upload">
+    <button @click="downloadExcel">下载</button>
     <div style="display: flex;width: 300px;margin-top: 30px; margin-left: auto; margin-right: auto; background-color: #409eff;">
       <div style="color: white">数值</div>
       <div style="margin-left: 180px;color: white">数量</div>
@@ -24,7 +25,10 @@
   export default {
     data() {
       return {
-        outputs: []
+        outputs: [],
+        excelData: [],
+        selectData: [],
+        allNumber: 0
       }
     },
     mounted() {
@@ -53,6 +57,7 @@
           const wsname = workbook.SheetNames[0];//取第一张表
           const ws = XLSX.utils.sheet_to_json(workbook.Sheets[wsname]);//生成json表格内容
           let valueNumber = {}
+          this.allNumber = ws.length-1
           for (var i = 1; i < ws.length; i++) {
             let value = ws[i].__EMPTY_1
             if (!valueNumber[value]) {
@@ -63,18 +68,54 @@
           }
           that.outputs = [];//清空接收数据
           for (var key in valueNumber) {
+
+            let count = valueNumber[key]
+            let rate = count/this.allNumber
             var sheetData = {
               testData: key,
-              count: valueNumber[key]
+              count: count,
+              all: this.allNumber,
+              rate: rate,
+              f: key * rate
             }
             that.outputs.push(sheetData);
           }
+          that.selectData = that.outputs
+          that.excelData = that.outputs
           console.log('result---------->', that.outputs)
           this.$refs.upload.value = '';
         };
         fileReader.readAsBinaryString(files[0]);
-      }
-    },
+      },
+      export2Excel() {
+        var that = this;
+        require.ensure([], () => {
+          const { export_json_to_excel } = require('@/excel/export2Excel'); //这里必须使用绝对路径，使用@/+存放export2Excel的路径
+          const tHeader = ['数值（x）','数量','样本总数','频率f(x)','xf(x)']; // 导出的表头名信息
+          const filterVal = ['testData','count', 'all', 'rate','f']; // 导出的表头字段名，需要导出表格字段名
+          const list = that.excelData;
+          const data = that.formatJson(filterVal, list);
+
+          export_json_to_excel(tHeader, data, '下载数据');// 导出的表格名称，根据需要自己命名
+        })
+      },
+      downloadExcel() {
+        this.excelData = this.selectData //你要导出的数据list。
+        this.export2Excel()
+        // this.$confirm('确定下载列表文件?', '提示', {
+        //   confirmButtonText: '确定',
+        //   cancelButtonText: '取消',
+        //   type: 'warning'
+        // }).then(() => {
+        //   this.excelData = this.selectData //你要导出的数据list。
+        //   this.export2Excel()
+        // }).catch(() => {
+        // })
+      },
+      formatJson(filterVal, jsonData) {
+        return jsonData.map(v => filterVal.map(j => v[j]))
+      },
+  }
   }
 </script>
 
